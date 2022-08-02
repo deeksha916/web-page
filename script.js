3e10b88f1184f977bfa2a65bf98a0117c8a3d48c
Deeksha Katare <60043009+deeksha916@users.noreply.github.com>
2022-08-02 16:29:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//var uName = prompt("name?")
+//console.log(uName)
+//var age = prompt("age?")
+//console.log(age)
+//var user1 = prompt("user1")
+//var user2 = prompt("user2")
+//if (user1 == user2) {
+//  console.log(user1)
+//}
+var age = prompt("age")
+Number('23')
+Number('deeksha')
